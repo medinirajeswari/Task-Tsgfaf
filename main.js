8ad333ba99abd6eab1c86cab9a2392bb9ab29f66
medinirajeswari <146118092+medinirajeswari@users.noreply.github.com>
2023-10-04 22:00:02 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 const app = express();
+const port = 3000;
 
 
 app.use(bodyParser.json());
